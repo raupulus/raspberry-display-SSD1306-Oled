a0a6202749d620b055cdd2d41217972e99581494
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-03-27 02:24:02 +0200

    Creado script para demo

---
+++ demo.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+
+# @author     Raúl Caro Pastorino
+# @copyright  Copyright © 2018 Raúl Caro Pastorino
+# @license    https://wwww.gnu.org/licenses/gpl.txt
+# @email      tecnico@fryntiz.es
+# @web        www.fryntiz.es
+# @github     https://github.com/fryntiz
+# @gitlab     https://gitlab.com/fryntiz
+# @twitter    https://twitter.com/fryntiz
+
+# Guía de estilos aplicada: PEP8
+
+#######################################
+# #           Descripción           # #
+#######################################
+
+
+#######################################
+# #       Importar Librerías        # #
+#######################################
+import setup.py
+
+
+#######################################
+# #            Constantes           # #
+#######################################
+
+
+#######################################
+# #             Variables           # #
+#######################################
+
+
+#######################################
+# #             Funciones           # #
+#######################################
