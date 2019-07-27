3b632eda16a8cf017c74cbde10e79fcbfe1a22ec
Raúl Caro Pastorino <tecnico@fryntiz.es>
2019-07-27 19:29:42 +0200

    Controlo excepciones y agrego archivo solo para información de la RPI

--- Oledssd1306.py
+++ Oledssd1306.py
@@ -2,7 +2,7 @@
 # -*- encoding: utf-8 -*-
 
 # @author     Raúl Caro Pastorino
-# @copyright  Copyright © 2018 Raúl Caro Pastorino
+# @copyright  Copyright © 2019 Raúl Caro Pastorino
 # @license    https://wwww.gnu.org/licenses/gpl.txt
 # @email      dev@fryntiz.es
 # @web        https://fryntiz.es
